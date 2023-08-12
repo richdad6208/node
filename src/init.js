1dde95451a45248c9a9c25b9765cef800498368e
Jaesung Park <richdad6208@gmail.com>
2023-08-12 12:07:18 +0900

    bugfix port apply

--- src/init.js
+++ src/init.js
@@ -4,7 +4,7 @@ import "./models/User";
 import "./db";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () =>
   console.log(`⭕ Now You Connected PORT: ${PORT} SERVER`)
 );
