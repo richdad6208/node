3be5216cf56bc0d0219978381c4bad1bb531269a
Jaesung Park <richdad6208@gmail.com>
2023-08-17 07:40:48 +0900

    bug fix

--- src/middlewares.js
+++ src/middlewares.js
@@ -12,13 +12,11 @@ const s3 = new S3Client({
 });
 const herokuUploadImage = multerS3({
   s3: s3,
-  // bucket: "richdad6208node/image",
   bucket: "richdad6208node",
   acl: "public-read",
 });
 const herokuUploadVideo = multerS3({
   s3: s3,
-  // bucket: "richdad6208node/video",
   bucket: "richdad6208node",
   acl: "public-read",
 });
