36a2680391082810a21d81b5944945be708a7f49
Jaesung Park <richdad6208@gmail.com>
2023-08-24 07:30:36 +0900

    가짜댓글 생성완료

--- src/client/js/comment.js
+++ src/client/js/comment.js
@@ -2,26 +2,35 @@ const formBtn = document.querySelector(".video__comment button");
 const form = document.querySelector(".video__comment");
 const textarea = form.querySelector("textarea");
 const videoPlay = document.querySelector(".video__play");
-formBtn.addEventListener("click", (e) => {
+
+function fakeComment(text) {
+  const commentList = document.querySelector(".video__comment__list");
+  const newCommentItem = document.createElement("li");
+  newCommentItem.className = "video__comment__item";
+  newCommentItem.innerText = text;
+  commentList.prepend(newCommentItem);
+}
+
+formBtn.addEventListener("click", async (e) => {
   e.preventDefault();
   const text = textarea.value;
   const videoId = videoPlay.dataset.id;
-  console.log("comment.js", text);
   if (text === "") {
     return;
   }
-  fetch(`/api/video/${videoId}/comment`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify({
-      text,
-    }),
-  })
-    .then(() => {
-      textarea.value = "";
-      window.location.reload();
-    })
-    .catch((err) => console.log(err));
+  try {
+    await fetch(`/api/video/${videoId}/comment`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        text,
+      }),
+    });
+    fakeComment(text);
+    textarea.value = "";
+  } catch (err) {
+    console.log(err);
+  }
 });
